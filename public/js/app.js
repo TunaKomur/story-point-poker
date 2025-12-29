069970677b9847eaf038dc24c6b82f6a7bb12871
maguro <140434036+Magurosekitan@users.noreply.github.com>
2025-12-29 08:23:51 +0300

    ford

--- public/js/app.js
+++ public/js/app.js
@@ -22,8 +22,8 @@
 
     let chart = null;
 
-    //const cardValues = ["0", "1/2", "1", "2", "3", "5", "8", "13", "21", "34", "55", "?"];
-    const cardValues = ["Tuna", "Kader", "Halit", "Ali", "Anıl", "Cihan", "Selin", "Mete", "Yaren", "Arda", "Aybars", "?"];
+    const cardValues = ["0", "1/2", "1", "2", "3", "5", "8", "13", "21", "34", "55", "?"];
+    //const cardValues = ["Tuna", "Kader", "Halit", "Ali", "Anıl", "Cihan", "Selin", "Mete", "Yaren", "Arda", "Aybars", "?"];
     let myName = null;
     let myRole = "user";
     let adminTaken = false;
